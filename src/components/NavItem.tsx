8d6179cb067c0007d9f1813f8229fa1589b0e9ff
mrbartchak <matthewbartchak@gmail.com>
2025-02-26 00:40:10 -0500

    highlight current path on nav list

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavItem = ({ href, label }: { href: string; label: string }) => {
+    const pathname = usePathname();
+    const isActive = pathname === href;
+
     return (
       <Link 
         href={href} 
-        className="block text-2xl font-bold mt-2 p-2 border border-foreground rounded-md hover:text-background hover:bg-foreground transition-colors duration-200"
+        className={`block text-2xl font-bold mt-2 p-3 rounded-lg transition-colors duration-300 shadow-[0_0_5px_rgba(0,0,0,0.15)] ${isActive ? 'bg-foreground text-background' : 'hover:bg-foreground hover:text-background'}`}
       >
         {label}
       </Link>
