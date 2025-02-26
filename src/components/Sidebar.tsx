206705afa2fb2d44452c6f14c90a3c09fb6c98e5
mrbartchak <matthewbartchak@gmail.com>
2025-02-26 00:40:23 -0500

    ajust alignment

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 const Sidebar = () => {
     return (
-      <div className="flex flex-col justify-start p-4 pt-36 pl-20 w-104 bg-transparent">
+      <div className="flex flex-col justify-start p-4 pt-44 pl-24 w-104 bg-transparent">
         <Image src="/profile.jpg" alt="Profile" className="rounded-full w-64 h-64 mb-4 drop-shadow-lg" width={3021} height={3021} quality={100} />
-        <h2 className="text-4xl font-bold text-foreground mb-2">Matthew Bartchak</h2>
+        <h2 className="text-4xl font-bold text-foreground mt-4 mb-2">Matthew Bartchak</h2>
         <p className="text-xl text-foreground text-stone-600 flex items-center gap-2">
             <Image src="/user.svg" alt="Location" className="w-5 h-5 opacity-60" width={16} height={16} quality={100} />
             Developer
@@ -14,7 +14,7 @@ const Sidebar = () => {
             <Image src="/location.svg" alt="Location" className="w-5 h-5 opacity-60" width={16} height={16} quality={100} />
             New York, New York
         </p>
-        <nav className="flex flex-col mt-4">
+        <nav className="flex flex-col mt-8">
         <NavItem href="/" label="Home" />
           <NavItem href="/about" label="About" />
           <NavItem href="/resume" label="Resume" />
