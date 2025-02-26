a6c662625158bff71bea46b99a29b4ed42fcb03d
mrbartchak <matthewbartchak@gmail.com>
2025-02-26 18:12:29 -0500

    image reformatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen items-center justify-center p-4 mx-2">
+    <main className="flex flex-col h-screen items-center justify-center p-4 mx-2">
       {/* Hero Section */}
       <div className="bg-background shadow-[0_0_16px_rgba(0,0,0,0.2)] rounded-lg p-6 max-w-5xl w-full h-1/3 mx-auto mb-8">
         <h1 className="text-xl font-bold mb-4 lg:text-3xl">Hello, I&apos;m Matthew Bartchak</h1>
@@ -28,7 +28,7 @@ export default function Home() {
               <p className="text-sm lg:text-lg text-gray-700">IT Analyst at</p>
               <p className="text-sm lg:text-lg text-gray-700">SMBC</p>
             </div>
-            <Image src="smbc-logo.svg" alt="SMBC Logo" className="w-32 h-32 lg:w-48 lg:h-48 -mt-12 -mb-12" width={48} height={48} />
+            <Image src="images/logos/smbc-logo.svg" alt="SMBC Logo" className="w-32 h-32 lg:w-48 lg:h-48 -mt-12 -mb-12" width={48} height={48} />
           </div>
 
           {/* Card B */}
@@ -38,7 +38,7 @@ export default function Home() {
               <p className="text-sm lg:text-lg text-gray-700">Computer Science and Business</p>
               <p className="text-sm lg:text-lg text-gray-700">Lehigh University</p>
             </div>
-            <Image src="lehigh-logo.svg" alt="SMBC Logo" className="w-48 h-48 lg:w-64 lg:h-64 -m-16 -mr-12" width={48} height={48} />
+            <Image src="images/logos/lehigh-logo.svg" alt="SMBC Logo" className="w-48 h-48 lg:w-64 lg:h-64 -m-16 -mr-12" width={48} height={48} />
           </div>
         </div>
 
@@ -47,28 +47,28 @@ export default function Home() {
           <h2 className="text-lg lg:text-2xl font-bold mb-6">Technical Skills</h2>
           <ul className="grid grid-cols-2 gap-4 lg:grid-cols-1">
             <li className="flex items-center mb-4 text-orange-700">
-              <Image src="java-icon.svg" alt="Java" className="w-8 h-8 mr-2" width={32} height={32} />
+              <Image src="images/icons/java-icon.svg" alt="Java" className="w-8 h-8 mr-2" width={32} height={32} />
               Java
             </li>
             <li className="flex items-center mb-4 text-black">
-              <Image src="nextjs-icon.svg" alt="Next.js" className="w-8 h-8 mr-2" width={32} height={32} />
+              <Image src="images/icons/nextjs-icon.svg" alt="Next.js" className="w-8 h-8 mr-2" width={32} height={32} />
               Next.js
             </li>
             <li className="flex items-center mb-4 text-sky-400">
-              <Image src="sql-icon.svg" alt="SQL" className="w-8 h-8 mr-2" width={32} height={32} />
+              <Image src="images/icons/sql-icon.svg" alt="SQL" className="w-8 h-8 mr-2" width={32} height={32} />
               SQL
             </li>
 
             <li className="flex items-center mb-4 text-lime-600">
-              <Image src="spring-icon.svg" alt="Spring Boot" className="w-8 h-8 mr-2" width={32} height={32} />
+              <Image src="images/icons/spring-icon.svg" alt="Spring Boot" className="w-8 h-8 mr-2" width={32} height={32} />
               Spring Boot
             </li>
             <li className="flex items-center mb-4 text-blue-500">
-              <Image src="azure-icon.svg" alt="Azure APIM" className="w-8 h-8 mr-2" width={32} height={32} />
+              <Image src="images/icons/azure-icon.svg" alt="Azure APIM" className="w-8 h-8 mr-2" width={32} height={32} />
               Azure APIM
             </li>
             <li className="flex items-center mb-4 text-red-600">
-              <Image src="git-icon.svg" alt="Git" className="w-8 h-8 mr-2" width={32} height={32} />
+              <Image src="images/icons/git-icon.svg" alt="Git" className="w-8 h-8 mr-2" width={32} height={32} />
               Git
             </li>
           </ul>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,15 +5,15 @@ const Sidebar = () => {
     return (
       <div className="flex flex-col p-4 pt-24 w-full bg-transparent lg:justify-start lg:pl-12 lg:w-96"> {/* Full Sidebar */}
         <div className="flex flex-row lg:flex-col lg:items-center"> {/* Profile photo + Name + Title */}
-          <Image src="/profile.jpg" alt="Profile" className="rounded-full lg:w-64 lg:h-64 w-32 h-32 mb-2 mr-2 shadow-2xl" width={3021} height={3021} quality={100} />
+          <Image src="/images/profile.jpg" alt="Profile" className="rounded-full lg:w-64 lg:h-64 w-32 h-32 mb-2 mr-2 shadow-2xl" width={3021} height={3021} quality={100} />
           <div> {/* Name + Title */}
             <h2 className="text-3xl font-bold text-foreground mt-4 mb-1">Matthew Bartchak</h2>
             <p className="text-xl text-foreground text-stone-500 flex items-center gap-2">
-                <Image src="/user.svg" alt="Location" className="w-5 h-5 opacity-50" width={16} height={16} quality={100} />
+                <Image src="images/icons//user-icon.svg" alt="Location" className="w-5 h-5 opacity-50" width={16} height={16} quality={100} />
                 Developer
             </p>
             <p className="text-xl text-foreground text-stone-500 flex items-center gap-2">
-                <Image src="/location.svg" alt="Location" className="w-5 h-5 opacity-50" width={16} height={16} quality={100} />
+                <Image src="/images/icons/pin-icon.svg" alt="Location" className="w-5 h-5 opacity-50" width={16} height={16} quality={100} />
                 New York, New York
             </p>
           </div>
