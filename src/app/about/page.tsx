6f6819f84b82d5dd835fdf47a199938aea92f64a
mrbartchak <matthewbartchak@gmail.com>
2025-02-26 00:25:32 -0500

    adding page routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <main className="flex min-h-screen items-center justify-center">
+        <h1 className="text-4xl font-bold">About</h1>
+      </main>
+    );
+  }
+  
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+      <main className="flex min-h-screen items-center justify-center">
+        <h1 className="text-4xl font-bold">Contact</h1>
+      </main>
+    );
+  }
+  
