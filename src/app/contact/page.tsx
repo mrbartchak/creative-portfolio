4cf6958cae1dca3c5ef5dd7ba17749f2f16cfb68
mrbartchak <matthewbartchak@gmail.com>
2025-02-28 13:17:00 -0500

    contact page links

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,18 +2,21 @@ import { ContactCard } from "@/components/cards";
 
 export default function Contact() {
     return (
-      <main className="flex flex-col h-full items-center justify-center space-y-4 border border-red-500">
-        <ContactCard 
+      <main className="flex flex-col h-full items-center justify-center space-y-4">
+        <ContactCard
+          link="mailto:matthewbartchak@gmail.com"
           imageSrc="images/icons/gmail-icon.svg"
           altText="Email icon"
           contact="matthewbartchak@gmail.com"
         />
-        <ContactCard 
+        <ContactCard
+          link="https://www.linkedin.com/in/mbartchak001/"
           imageSrc="images/icons/linkedin-icon.svg"
           altText="Linkedin icon"
           contact="Linkedin"
         />
-        <ContactCard 
+        <ContactCard
+          link="https://github.com/mrbartchak"
           imageSrc="images/icons/github-icon.svg"
           altText="Github icon"
           contact="Github"
--- src/components/cards/ContactCard.tsx
+++ src/components/cards/ContactCard.tsx
@@ -1,19 +1,27 @@
 import Image from "next/image";
 
 interface ContactCardProps {
+    link: string;
     imageSrc: string;
     altText: string;
     contact: string;
 }
 
-const ContactCard = ({ imageSrc, altText, contact }: ContactCardProps) => {
+const ContactCard = ({link, imageSrc, altText, contact }: ContactCardProps) => {
 	return (
-		<div className="flex justify-between items-center bg-none rounded-lg p-4 w-full shadow-[0_0_16px_rgba(0,0,0,0.2)] ">
-            <Image src={imageSrc} alt={altText} className="h-12 lg:h-20 w-auto" width={32} height={32} />
-            <div>
-			    <p className="lg:text-xl text-foreground opacity-90">{contact}</p>
+        <a  href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-full"
+        >
+            <div className="flex justify-between items-center bg-none rounded-lg p-4 w-full shadow-[0_0_16px_rgba(0,0,0,0.2)]
+                            transition-transform duration-300 ease-in-out hover:scale-105">
+                <Image src={imageSrc} alt={altText} className="h-12 lg:h-20 w-auto" width={32} height={32} />
+                <div>
+                    <p className="lg:text-xl text-foreground opacity-90">{contact}</p>
+                </div>
             </div>
-		</div>
+        </a>
 	);
 }
 export default ContactCard;
